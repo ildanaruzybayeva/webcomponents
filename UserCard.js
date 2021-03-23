81ebad3cb94683732c2fc4629515f48f4ff33745
Ildana <ildana.ruzybayeva@gmail.com>
2021-03-23 11:53:04 +0000

    Added Javascript and another prop

--- UserCard.js
+++ UserCard.js
@@ -1,7 +1,27 @@
+const template = document.createElement('template')
+template.innerHTML = `
+<style>
+    button {
+        color: coral
+    }
+</style>
+<div class="user-card">
+    <img />
+    <div>
+        <button></button>
+    </div>
+</div>
+
+`
+
 class UserCard extends HTMLElement {
     constructor(){
         super()
-        this.innerHTML = `hello blin`
+        this.attachShadow({mode: 'open'})
+        this.shadowRoot.appendChild(template.content.cloneNode(true))
+        this.shadowRoot.querySelector('button').innerText = this.getAttribute('name')
+        this.shadowRoot.querySelector('img').src = this.getAttribute('avatar')
+        this.shadowRoot.addEventListener('click', ()=> alert(`clicked on ${this.getAttribute('name')}`))
     }
 }
 
